fcf7637706e6a6d8d5c3a3d9ce8df3477bde31df
WolfgangPohrt <kouzelhs.th@gmail.com>
2022-02-16 19:06:07 +0200

    cleaning eval metric

--- evaluate_metric.py
+++ evaluate_metric.py
@@ -1,7 +1,7 @@
 
 from sqlite3 import Timestamp
 import pysrt 
-from der import DER, build_cost_matrix
+from tools.der import build_cost_matrix
 from eval_metrics import evaluate_metrics_from_lists
 
 def to_seconds(timestamp):
@@ -21,12 +21,23 @@ def hyp_srt_to_timestamps(srt_path):
     return subs
 
 def get_duration(ref_timestamps):
+    """
+    Get duration of Movie/Video.
+    
+    !Currently assuming duration
+    is equal to the end time of the
+    last subtitle.!
+    
+    !!!todo get accual duration todo!!!
+    """
     return to_seconds(ref_timestamps[-1].end)
 
 
 
 def prep_timestamps(subs, duration):
-    
+    """
+    Reforamt subs from pysrt format.
+    """
     start_time_sp = .0
     timestamps_final = []
     for i, sub in enumerate(subs):
@@ -40,6 +51,10 @@ def prep_timestamps(subs, duration):
     return timestamps_final
 
 def matched_captions(ref, hyp):
+    """
+    Find the verlaping ref and hyp captions
+    and return a list of tuples (ref,hyp)
+    """
     matched_caps = []
     for h in hyp:
         s_h, e_h, cap_h = h[1:]
@@ -57,7 +72,7 @@ def calculate_spider(matched_caps):
     m, _ = evaluate_metrics_from_lists(hyp, ref, ids)
     return m['SPIDEr']
 
-def my_metric(cost_matrix):
+def my_metric(ref_srt, hyp_srt):
     """
     cost_matrix: a 2-dim numpy array:
         elem (0,0) --> correct nosp
@@ -65,36 +80,31 @@ def my_metric(cost_matrix):
         elem (1,0) --> false alarm
         elem (1,1) --> correct sp
      """
-    spider = 0.5
+    ref = ref_srt_to_timestamps('/home/theokouz/src/audio_desc_pipeline/subs_scraping/srt_files/Blade.Runner.Black.Lotus.S01E07.WEBRip.x264-ION10.srt')
+    duration = (get_duration(ref))
+    ref = prep_timestamps(ref, duration)
+    hyp = hyp_srt_to_timestamps('/home/theokouz/src/audio_desc_pipeline/test_subs.srt')
+    hyp = prep_timestamps(hyp, duration)
+    cost_matrix = build_cost_matrix(ref, hyp)
+
+
+    ref_nosp = [r for r in ref if r[0] == 'nosp']
+    hyp_nosp = [h for h in hyp if h[0] == 'nosp']
+    matched_caps = matched_captions(ref_nosp, hyp_nosp)
+    spider = calculate_spider(matched_caps)
+
     total_ref_time = cost_matrix[1,0] + cost_matrix[1,1]
     confusion = cost_matrix[0,0] * (1 - spider)
     metric = (cost_matrix[1,0] + cost_matrix[0,1] + confusion) / total_ref_time
     return metric
 
-ref = ref_srt_to_timestamps('/home/theokouz/src/audio_desc_pipeline/subs_scraping/srt_files/Blade.Runner.Black.Lotus.S01E07.WEBRip.x264-ION10.srt')
-duration = (get_duration(ref))
-ref = prep_timestamps(ref, duration)
-hyp = hyp_srt_to_timestamps('/home/theokouz/src/audio_desc_pipeline/test_subs.srt')
-hyp = prep_timestamps(hyp, duration)
-# hyp = [('nosp', 0., 0.5), ('sp', 0.5, 1248.)]
 
-ref_nosp = [r for r in ref if r[0] == 'nosp']
-hyp_nosp = [h for h in hyp if h[0] == 'nosp']
+if __name__ == '__main__':
 
 
+    ref_srt = '/home/theokouz/src/audio_desc_pipeline/subs_scraping/srt_files/Blade.Runner.Black.Lotus.S01E07.WEBRip.x264-ION10.srt'
+    hyp_srt = '/home/theokouz/src/audio_desc_pipeline/test_subs.srt'
 
 
-# total_time = sum([e-s for _, s, e, txt in nosp_intervals])
-cost_matrix = build_cost_matrix(ref, hyp)
+    print(my_metric(ref_srt, hyp_srt))
 
-# r = ['hitler as kalki']
-# h = [['hitler as kalki', 'hitler as kalki', 'hitler as kalki', 'hitler as kalki', 'hitler as kalki']]
-# m = evaluate_metrics_from_lists(r,h,[1])
-# print(m)
-
-matched_caps = matched_captions(ref_nosp, hyp_nosp)
-spider = calculate_spider(matched_caps)
-print(spider)
-# print(my_metric(cost_matrix))
-# print(ref)
-# print(hyp)
